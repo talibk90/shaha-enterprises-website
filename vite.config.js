3013d78e00482e35dcb84f5e9277687e3aeb5c82
Talib khan <talibasus90@gmail.comm>
2025-09-12 20:13:14 +0530

    Fix deployment configuration and add GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shaha-enterprises-website/',
   server: {
     host: true,
     port: 3000,
